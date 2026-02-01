a3541bef772beb14757b7a93eea01c03a8bcec3b
ubvdesk-droid <ubv.desk@gmail.com>
2026-02-01 22:21:29 +0200

    Update opp.py

--- opp.py
+++ opp.py
@@ -37,12 +37,13 @@ if prompt := st.chat_input("כתוב משהו..."):
         st.markdown(prompt)
 
     # שליחה למודל של גוגל
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-1.5-flash-latest')
     response = model.generate_content(prompt)
     
     with st.chat_message("assistant"):
         st.markdown(response.text)
     st.session_state.messages.append({"role": "assistant", "content": response.text})
+
 
 
 
